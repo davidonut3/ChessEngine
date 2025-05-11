c95877d38bcb357b855d775accafafa600f38bae
davidonut3 <david@martinengeanne.nl>
2025-05-11 22:31:34 +0200

    More unknown changes

---
+++ target/release/build/proc-macro2-028a8513cb9107f3/build_script_build-028a8513cb9107f3.d
@@ -0,0 +1,5 @@
+C:\Users\spele\source\repos\ChessEngine\target\release\build\proc-macro2-028a8513cb9107f3\build_script_build-028a8513cb9107f3.exe: C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.95\build.rs
+
+C:\Users\spele\source\repos\ChessEngine\target\release\build\proc-macro2-028a8513cb9107f3\build_script_build-028a8513cb9107f3.d: C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.95\build.rs
+
+C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.95\build.rs:
---
+++ target/release/build/pyo3-build-config-543d76003062a424/build_script_build-543d76003062a424.d
@@ -0,0 +1,9 @@
+C:\Users\spele\source\repos\ChessEngine\target\release\build\pyo3-build-config-543d76003062a424\build_script_build-543d76003062a424.exe: C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\build.rs C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\impl_.rs C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\errors.rs
+
+C:\Users\spele\source\repos\ChessEngine\target\release\build\pyo3-build-config-543d76003062a424\build_script_build-543d76003062a424.d: C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\build.rs C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\impl_.rs C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\errors.rs
+
+C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\build.rs:
+C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\impl_.rs:
+C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pyo3-build-config-0.24.2\src\errors.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.24.2
