584cf8b18ca8e9be49b9710cd0066c67a327552e
davidonut3 <david@martinengeanne.nl>
2025-05-11 22:50:11 +0200

    Removed target/ from all files in main branch

--- target/release/deps/cfg_if-22491faf1dc7620d.d
+++
@@ -1,7 +0,0 @@
-C:\Users\spele\source\repos\ChessEngine\target\release\deps\libcfg_if-22491faf1dc7620d.rmeta: C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\spele\source\repos\ChessEngine\target\release\deps\libcfg_if-22491faf1dc7620d.rlib: C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\spele\source\repos\ChessEngine\target\release\deps\cfg_if-22491faf1dc7620d.d: C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\spele\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs:
